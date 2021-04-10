1aa9f0da7706104ddd351426472ef8c484ccac4c
Hakan Erünsal <hakanerunsal@windowslive.com>
2021-04-10 19:36:22 +0300

    Update index.php

--- index.php
+++ index.php
@@ -1,2 +1,3 @@
 <?php
-echo " https://play.google.com/store/apps/details?id=com.rigbak.adnam2";
+echo " https://play.google.com/store/apps/details?id=com.rigbak.adnam2
+https://apps.apple.com/us/app/justice-gun-2/id1505399592";
